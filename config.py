3cfb8f884551066f3de2d3b09526d58db6090153
Eduardo Rangel <111319309+EddyBel@users.noreply.github.com>
2025-04-26 19:40:23 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ app_port = int(os.environ.get('PORT', '5000'))
 
 db_host = os.environ.get('MYSQL_HOST', 'localhost')
 db_user = os.environ.get('MYSQL_USER', 'root')
-db_password = os.environ.get('MYSQL_PASSWORD', '54628')
+db_password = os.environ.get('MYSQL_PASSWORD', '54321')
 db_name = os.environ.get('MYSQL_DB', 'matzz')
 db_port = os.environ.get('MYSQL_PORT', '3306')
 
